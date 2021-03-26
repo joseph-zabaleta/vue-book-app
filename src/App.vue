f6387670536dd9e2cc50770bc0f08fb987a7046d
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2021-03-26 00:36:14 -0700

    Starter Navigation Completed

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-	<div>
-		App
-	</div>
+	<v-app>
+		<NavBar></NavBar>
+	</v-app>
 </template>
 
 <script>
+import NavBar from './components/NavBar.vue';
+
 export default {
 	name: 'App',
+	components: {
+		NavBar,
+	},
 };
 </script>
 
---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<v-app-bar dense dark color="black">
+			<v-toolbar-title>The Famous Book App</v-toolbar-title>
+
+			<v-spacer></v-spacer>
+
+			<v-menu left bottom>
+				<template v-slot:activator="{ on, attrs }">
+					<v-btn icon v-bind="attrs" v-on="on">
+						<v-app-bar-nav-icon></v-app-bar-nav-icon>
+					</v-btn>
+				</template>
+
+				<v-list>
+					<v-list-item
+						v-for="(option, idx) in this.navOptions"
+						:key="idx"
+						@click="() => {}"
+					>
+						<v-list-item-title>{{ option }}</v-list-item-title>
+					</v-list-item>
+				</v-list>
+			</v-menu>
+		</v-app-bar>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'NavBar',
+	data() {
+		return {
+			navOptions: ['Search', 'My Collection'],
+		};
+	},
+};
+</script>
+
+<style scoped></style>
